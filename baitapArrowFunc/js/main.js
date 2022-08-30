f98470739c506bb93efd99ae35b0cb0dfce1f3c9
chungminhluan <luanluanluan200797@gmail.com>
2022-08-30 20:46:58 +0700

    add view index

--- baitapArrowFunc/js/main.js
+++ baitapArrowFunc/js/main.js
@@ -1,18 +1,71 @@
-let colorList = ["pallet", "viridian", "pewter", "cerulean", "vermillion", "lavender", "celadon", "saffron", "fuschia", "cinnabar"];
+// let colorList = ["pallet", "viridian", "pewter", "cerulean", "vermillion", "lavender", "celadon", "saffron", "fuschia", "cinnabar"];
 
-let renderButton = (colorContainer) => {
-    let output = '';
-    for (let i = 0; i < colorContainer.length; i++) {
-        let color = colorContainer[i];
-        output += `
-        <button class="color-button ${color} active" onclick="changeColor('${color}')">${color}</button>
-        `
-    }
-    document.querySelector('#colorContainer').innerHTML = output;
-}
+// let renderButton = (colorContainer) => {
 
-renderButton(colorList);
-let changeColor = (color) => {
-    document.querySelector('#house') = color;
+//     for (let i = 0; i < colorContainer.length; i++) {
+//         let color = colorContainer[i];
+//         document.querySelector('#colorContainer').innerHTML += `
+//         <button class="color-button ${color} active" onclick="changeColor('${color}')">${color}</button>
+//         `
+//     }
 
-}
+// }
+
+// renderButton(colorList);
+// let changeColor = (color) => {
+//     document.querySelector('#house') = color;
+
+// }
+
+
+// const colorList = ["pallet", "viridian", "pewter", "cerulean", "vermillion", "lavender", "celadon", "saffron", "fuschia", "cinnabar"];
+// let container = document.getElementById("colorContainer");
+// (loadColorPick = () => {
+//     for (let o = 0; o < colorList.length; o++)
+//         container.innerHTML +=
+//         0 == o ?
+//         "<button class='color-button " + colorList[o] + " active'></button>" :
+//         "<button class='color-button " + colorList[o] + "'></button>"
+
+// }), loadColorPick();
+// let colorPicker = document.getElementsByClassName("color-button"),
+//     house = document.getElementById("house");
+// for (let o = 0; o < colorPicker.length; o++) colorPicker[o].addEventListener("click", function () {
+//     changeColor(colorList[o], o)
+// });
+// changeColor = (o, e) => {
+//     for (let o = 0; o < colorPicker.length; o++) colorPicker[o].classList.remove("active");
+//     colorPicker[e].classList.add("active"), house.className = "house " + o
+// };
+const colorList = [
+    "pallet",
+    "viridian",
+    "pewter",
+    "cerulean",
+    "vermillion",
+    "lavender",
+    "celadon",
+    "saffron",
+    "fuschia",
+    "cinnabar",
+];
+let container = document.getElementById("colorContainer");
+window.loadColorPick = () => {
+        for (let o = 0; o < colorList.length; o++)
+            container.innerHTML +=
+            0 == o ?
+            "<button class='color-button " + colorList[o] + " active'></button>" :
+            "<button class='color-button " + colorList[o] + "'></button>";
+    },
+    loadColorPick();
+let colorPicker = document.getElementsByClassName("color-button"),
+    house = document.getElementById("house");
+for (let o = 0; o < colorPicker.length; o++)
+    colorPicker[o].addEventListener("click", function () {
+        changeColor(colorList[o], o);
+    });
+window.changeColor = (o, e) => {
+    for (let o = 0; o < colorPicker.length; o++)
+        colorPicker[o].classList.remove("active");
+    colorPicker[e].classList.add("active"), (house.className = "house " + o);
+};
---
+++ baitapRest/js/main.js
@@ -0,0 +1,17 @@
+ tinhDTB = ((...e) => {
+     let n = 0;
+     return e.map(e => {
+         n += parseFloat(e)
+     }), (n / e.length).toFixed(3)
+ }), document.getElementById("btnKhoi1").onclick = (() => {
+     let t = document.getElementById("inpToan").value,
+         l = document.getElementById("inpLy").value,
+         h = document.getElementById("inpHoa").value;
+     document.getElementById("tbKhoi1").innerHTML = tinhDTB(t, l, h)
+ }), document.getElementById("btnKhoi2").onclick = (() => {
+     let v = document.getElementById("inpVan").value,
+         s = document.getElementById("inpSu").value,
+         d = document.getElementById("inpDia").value,
+         e = document.getElementById("inpEnglish").value;
+     document.getElementById("tbKhoi2").innerHTML = tinhDTB(v, s, d, e)
+ });
